e07c980e24b74dd1082f8011a239b697458987b9
Mark Arce <marce@myriad.com>
2021-12-17 00:57:53 -0800

    day_14 part_1 complete

--- 2021/day_14/part_2.py
+++ 2021/day_14/part_2.py
@@ -1,8 +1,48 @@
+from collections import defaultdict
 
-
-def boilerplate(filename):
+def expand_polymer(filename):
     with open(filename) as f:
-        lines = f.readlines()
+        lines = [line.rstrip() for line in f.readlines()]
 
+    polymer_template = lines[0]
 
-boilerplate("sample_data.txt")
+    pair_insertion_rules = {}
+    for line in lines[2:]:
+        pair, letter = line.split(" -> ")
+        pair_insertion_rules[pair] = letter
+
+    def process_polymer(polymer, steps):
+        while steps > 0:
+            new_polymer = ""
+            last = polymer[-1]
+            print(polymer)
+            print(last)
+            for i in range(0, len(polymer) - 1):
+                pair = polymer[i: i + 2]
+                print(pair)
+                new_polymer += polymer[i] + pair_insertion_rules[pair]
+            new_polymer += last
+            print(new_polymer)
+            polymer = new_polymer
+            steps -= 1
+        return polymer
+
+    print(pair_insertion_rules)
+
+    polymer = process_polymer(polymer_template, 40)
+
+    letter_count = defaultdict(lambda: 0)
+
+    for letter in polymer:
+        letter_count[letter] += 1
+
+    print(letter_count)
+    highest = letter_count[max(letter_count, key=letter_count.get)]
+    lowest = letter_count[min(letter_count, key=letter_count.get)]
+
+    print(highest - lowest)
+    # print(lines)
+    # print(polymer_template)
+    # print(pair_insertion_rules)
+
+expand_polymer("data.txt")
