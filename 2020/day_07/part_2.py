694876bc752dbb4fde9c862c49b93da1cf92dce3
Mark Arce <mark.arce@gmail.com>
2022-03-11 09:12:20 -0800

    day_07 complete

--- 2020/day_07/part_2.py
+++ 2020/day_07/part_2.py
@@ -1,49 +1,29 @@
 def parse_bag_rules(file_location):  # /Users/marce/AdventOfCode/2020/bag_rules.txt
-    import re
-    with open(file_location) as f:
-        bag_rules = f.read().split(".\n")
+    from collections import deque
 
-    bag_rules[-1] = bag_rules[-1][:-1]
+
     bag_contents = {}
-    for rule in bag_rules:
-        container, contents = rule.split("s contain ")
-        if contents == "no other bags":
-            bag_contents[container] = None
+    with open(file_location) as f:
+        for line in f:
+            container, contents = line.split("s contain ")
+            if contents.rstrip() == "no other bags.":
+                bag_contents[container] = None
+            else:
+                bag_contents[container] = {bag[2:].rstrip("s."): int(bag[:1]) for bag in contents[:-1].split(", ")}
 
-        else:
-            contents = contents.split(", ")
-            formatted_contents = []
-            for bag_count_and_type in contents:
-                if bag_count_and_type[-1] == "s":
-                    bag_count_and_type = bag_count_and_type[:-1]
-                m = re.match(r"(\d+)\s(.*)", bag_count_and_type)
-                bag_count = m.group(1)
-                bag_type = m.group(2)
-                new_contents = {}
-                new_contents[bag_type] = bag_count
-                formatted_contents.append(new_contents)
-            bag_contents[container] = formatted_contents
-    return bag_contents
+    stack = deque()
+    bag_total = 0
+    for bag in bag_contents["shiny gold bag"].items():
+        stack.append((bag))
 
-def does_bag_hold_gold_bags(bag, all_bags):
-    # empty bag holds no bags
-    if all_bags[bag] is not None:
-        gold_bags_held = []
-        for element in all_bags[bag]:
-        # recursive case: bag contains 1 or more bags
-            for key in element.keys():
-        # one bag is a gold bag
-                if key == "shiny gold bag":
-                    gold_bags_held.append(True)
-                else:
-                    gold_bags_held.append(does_bag_hold_gold_bags(key, all_bags))
-        return any(gold_bags_held)
+    while stack:
+        print(stack)
+        current_bag, current_count = stack.pop()
+        bag_total += current_count
+        if bag_contents[current_bag] is not None:
+            for i in range(current_count):
+                stack.extend(bag for bag in bag_contents[current_bag].items())
 
-def bags_inside_bag(bag, all_bags):
-    bags_held = 0
-    if all_bags[bag] is not None:
-        for element in all_bags[bag]:
-            for key in element.keys():
-                bags_held += int(element[key])
-                bags_held += bags_inside_bag(key, all_bags)
-    return bags_held
+    print(bag_total)
+
+parse_bag_rules("bag_rules.txt")
